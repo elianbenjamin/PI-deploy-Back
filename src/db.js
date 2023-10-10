694cca400fc9e2f3ac7ae82eac5425e6dbc1961a
elianbenjamin <elianbenjamin30@gmail.com>
2023-10-10 01:29:35 -0300

    cambios de URL

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize('postgres://elian:eJRurQEhgGCvQVTQXUMGZZXD5XpRNocL@dpg-ckib72q12bvs73fecdk0-a/videogames_qjwq', {
+const sequelize = new Sequelize('postgres://elian:sTjf6ASSg3UYa4qEyLzGamVLrCvn9ZFv@dpg-ckid58a12bvs73fr58kg-a/videogames_xd9e', {
   /* postgres://elian:eJRurQEhgGCvQVTQXUMGZZXD5XpRNocL@dpg-ckib72q12bvs73fecdk0-a/videogames_qjwq */
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
